3df9a2456eaa325cf07066a1d0f990513481f1ad
eric6166 <ericnguyen6166@gmail.com>
2024-01-05 21:21:41 +0700

    epic/gateway-oauth2-server

--- src/main/java/com/example/userservice/validation/UserValidation.java
+++
@@ -1,8 +0,0 @@
-package com.example.userservice.validation;
-
-import com.example.springbootmicroservicesframework.dto.AccountDto;
-import com.example.springbootmicroservicesframework.exception.ValidationException;
-
-public interface UserValidation {
-    void validateAccountExisted(AccountDto account) throws ValidationException;
-}
